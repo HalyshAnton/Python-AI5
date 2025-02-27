12a0ef72d047c479b214b3d1958b1aac4821b3e5
HalyshAnton <kyhar007@gmail.com>
2025-02-27 19:49:18 +0200

    add Animal

--- main.py
+++ main.py
@@ -1,130 +1,126 @@
-# наслідування
+# # наслідування
+#
+# # переведення числа в діапазон [0, 100]
+#
+# value = -10
+#
+# # варіант через if
+# # if value > 100:
+# #     value = 100
+# # elif value < 0:
+# #     value = 0
+#
+# # через min max
+#
+# value = -10
+# new_value = min(value, 100)
+# new_value = max(new_value, 0)
+#
+# new_value = max(0, min(100, value))  # clip
+#
+# print(new_value)
 
-# class Parent:
-#     def __init__(self, name, age):
-#         self.name = name
-#         self.age = age
-#
-#     def show_info(self):
-#         print(f"клас Parent")
-#         print(f'{self.name}, {self.age} років')
-#
-#
-# class Child(Parent):
-#     def play(self):   # новий метод
-#         print('клас Child')
-#         print(f'{self.name} грається')
-#
-#     def show_info(self):  # перевизначений метод
-#         print(f"клас Child")
-#         print(f'{self.name}, {self.age} років')
-#
-#
-# class Daughter(Parent):
-#     def __init__(self, name, age, dream):
-#         self.name = name
-#         self.age = age
-#         self.dream = dream
-#
-#
-# # mother = Parent('Marry', 36)
-# # mother.show_info()
-# #
-# # print()
-# #
-# # child = Child('Rony', 8)
-# # child.show_info()
-# # child.play()
-#
-# # daughter = Daughter("Linda", 10, 'became a doctor')
-# # daughter.show_info()
-#
-# # код з класом Parent
-# mother = Parent('Marry', 36)
-# mother.show_info()
+# використання методів батьківського класу
+from abc import ABC, abstractmethod
+
+class Animal(ABC):  # абстрактний клас(не можна створити об'єкт)
+    @abstractmethod
+    def __init__(self, name, age):
+        self._check_name(name)
+        self._check_age(age)
+        self.name = name
+        self.age = age
+
+    def _check_name(self, name):
+        # перевірка чи тип даних str
+        if not isinstance(name, str):
+            raise ValueError(f"Ім'я має бути рядком, отримано тип {type(name)}")
+
+        # лише літери та символи ' ' '-'
+        for sym in name:
+            if not(sym.isalpha() or sym in ' -'):
+                raise ValueError("Ім'я має складатися лише з літер та ' -'")
+
+    def _check_age(self, age):
+        # перевірка чи тип даних int float
+        if not isinstance(age, (int, float)):
+            raise ValueError(f"Вік має бути числом, отримано тип {type(age)}")
+
+        if age <= 0 or age >= 20:
+            raise ValueError(f"Вік має бути в діапазоні [0, 20]")
 
 
+    def info(self):
+        print(f"Ім'я: {self.name}, {self.age} років")
 
-# класи для Транспортні засоби
 
-class Vehicle:
-    def __init__(self, owner,  # власник
-                 max_fuel_level,  # максимальний рівень пального
-                 milliage  # км / 1 літр пального
-                 ):
-        self.owner = owner
-        self.max_fuel_level = max_fuel_level
-        self.fuel_level = max_fuel_level # бак повний
-        self.milliage = milliage
+class Cat(Animal): # name, age, is_vaccinated
+    def __init__(self, name, age, is_vaccinated=True):
+        super().__init__(name, age)
+        self.is_vaccinated = is_vaccinated
 
-    def move(self, speed, distance):
-        need_fuel = distance / self.milliage
+    def catch_mouse(self):
+        print("Ловить мишу")
 
-        if self.fuel_level < need_fuel: # не хватає пального
-            print('не хватає пального')
+    def info(self):  # додатково писало що це кіт
+        print("Кіт")
+        # super() # super -- батьківський клас
+        super().info() # info з класу Animal
+
+        if self.is_vaccinated:
+            print("Вакцинований")
         else:
-            self.fuel_level -= need_fuel
-            time = distance / speed
-
-            print(f"Проїхали {distance}км за {time}год")
+            print("Потрібно вакцинувати")
 
 
-    def add_fuel(self, fuel):
-        self.fuel_level += fuel
+# cat1 = Cat('Tom', 5)
+# cat1.info()
+# #cat1.catch_mouse()
+# print()
+#
+# cat = Animal('Roger', 10)
+# cat.info()
 
-class Car(Vehicle):
+# приховані атрибути\методи
+
+class Cat(Animal): # name, age, is_vaccinated
+    def __init__(self, name, age, is_vaccinated=True):
+        super().__init__(name, age)
+        self._is_vaccinated = is_vaccinated  # прихований атрибут
+
+    def catch_mouse(self):
+        print("Ловить мишу")
+
+    def info(self):  # додатково писало що це кіт
+        print("Кіт")
+        # super() # super -- батьківський клас
+        super().info() # info з класу Animal
+
+        if self._is_vaccinated:
+            print("Вакцинований")
+        else:
+            print("Потрібно вакцинувати")
+
+    def vaccinate(self):
+        self._is_vaccinated = True
+
+    def unvaccinate(self):
+        self._is_vaccinated = False
+
+
+class Kitten(Cat):
     pass
 
 
-class Bicycle(Vehicle):
-    def __init__(self, owner, max_fuel_level, milliage):
-        self.owner = owner
-        self.max_fuel_level = max_fuel_level
-        self.fuel_level = max_fuel_level  # бак повний
-        self.milliage = milliage
+cat1 = Cat('Tom', 2.5)
 
-        self.used_motor = False # мотор виключений
+cat1.info()
 
+cat1.unvaccinate()
 
-    def move(self, speed, distance):  # теж рухається але не трабе пального
-        if not self.used_motor:
-            time = distance / speed
-            print("Без пального")
-            print(f"Проїхали {distance}км за {time}год")
-            return
+cat1.info()
 
-        # мотор вклюсений(код з Vehicle.move)
+# print(cat1._Cat__is_vaccinated)
 
-        need_fuel = distance / self.milliage
-
-        if self.fuel_level < need_fuel:  # не хватає пального
-            print('не хватає пального')
-        else:
-            self.fuel_level -= need_fuel
-            time = distance / speed
-
-            print("З пальним")
-            print(f"Проїхали {distance}км за {time}год")
-
-    def turn_on(self):
-        self.used_motor = True
-
-    def turn_off(self):
-        self.used_motor = False
-
-
-class Plane(Vehicle):
-    pass
-
-
-
-bike = Bicycle('John', 100, 50)
-bike.move(20, # швидкість
-         15  # кілометри
-         )
-
-bike.turn_on()
-
-bike.move(40,
-          100)
-
+kitten = Kitten("Murchyck", 1)
+kitten.info()
