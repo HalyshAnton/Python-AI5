f4de7b6982548c702639b44adedc534aeb892114
HalyshAnton <kyhar007@gmail.com>
2025-03-06 15:06:30 +0200

    add hw

--- dz_class.py
+++ dz_class.py
@@ -1,90 +1,80 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-        self.prev = None
+class Task:
+    def __init__(self, name):
+        self.name = name
+        self.subtasks = []
 
-    def __str__(self):
-        return f"{self.data} -> {self.next}"
-
-
-class DoubleLinkedList:
-    """
-    Клас двозв'язного списку.
-    """
-
-    def __init__(self):
+    def do(self):
         """
-        Ініціалізація порожнього списку.
+        Виконує завдання, за потреби розбиває його на підзавдання
+        :return: список підзавдань
         """
-        self.head = None
-        self.tail = None
-
-    def __str__(self):
-        return str(self.head)
-
-    def push_end(self, data):
-        """
-        Додає елемент у кінець списку.
-        :param data: Дані для додавання
-        """
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
-            self.tail = new_node
+        if self.subtasks:
+            print(f"Виконую завдання: {self.name}. Розбиваю на підзавдання")
         else:
-            self.tail.next = new_node
-            new_node.prev = self.tail
-            self.tail = new_node
+            print(f"Завершено завдання: {self.name}")
 
-    def push_start(self, data):
-        """
-        Додає елемент на початок списку.
-        :param data: Дані для додавання
-        """
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.next = self.head
-            self.head.prev = new_node
-            self.head = new_node
+        return self.subtasks
 
-    def pop_end(self):
-        """
-        Видаляє останній елемент зі списку.
-        :return: Дані видаленого елемента або None, якщо список порожній
-        """
-        if not self.tail:
-            return None
 
-        data = self.tail.data
+class Project:
+    ## YOUR CODE
+    pass
 
-        if self.head.next is None:
-            self.head = None
-            self.tail = None
-        else:
-            self.tail = self.tail.prev
-            self.tail.next = None
 
-        return data
+task = Task('Підготовка до зйомок')
 
-    def pop_start(self):
-        """
-        Видаляє перший елемент зі списку.
-        :return: Дані видаленого елемента або None, якщо список порожній
-        """
+task.subtasks = [
+    Task('Пошук локацій'),
+    Task('Підготовка сценарію'),
+    Task('Кастинг акторів')
+]
 
-        if not self.head:
-            return None
+# Підзавдання для "Пошук локацій"
+task.subtasks[0].subtasks = [
+    Task('Огляд локацій у місті'),
+    Task('Огляд локацій за містом'),
+    Task('Узгодження місць для зйомок')
+]
 
-        data = self.head.data
+# Підзавдання для "Підготовка сценарію"
+task.subtasks[1].subtasks = [
+    Task('Написання основного сценарію'),
+    Task('Редагування сценарію'),
+    Task('Підготовка сценарних приміток'),
+]
 
-        if self.head.next is None:
-            self.head = None
-            self.tail = None
-        else:
-            self.head = self.head.next
-            self.head.prev = None
-        return data
+# Підзавдання для "Кастинг акторів"
+task.subtasks[2].subtasks = [
+    Task('Пошук головних акторів'),
+    Task('Пошук другорядних акторів'),
+    Task('Підготовка контрактів для акторів')
+]
+
+# Підзавдання для "Пошук локацій у місті"
+task.subtasks[0].subtasks[0].subtasks = [
+    Task('Вибір декорацій для зйомок'),
+    Task('Узгодження з власниками приміщень')
+]
+
+# Підзавдання для "Огляд локацій за містом"
+task.subtasks[0].subtasks[1].subtasks = [
+    Task('Вибір лісу для сцени битви'),
+    Task('Пошук старовинних будівель для сцени'),
+]
+
+# Підзавдання для "Написання основного сценарію"
+task.subtasks[1].subtasks[0].subtasks = [
+    Task('Написання першої частини'),
+    Task('Написання другої частини'),
+]
+
+# Підзавдання для "Пошук головних акторів"
+task.subtasks[2].subtasks[0].subtasks = [
+    Task('Пошук актора на роль головного героя'),
+    Task('Пошук актриси на роль головної героїні')
+]
+
+project = Project(task)
+
+while not project.is_finished():
+    project.do_task()
