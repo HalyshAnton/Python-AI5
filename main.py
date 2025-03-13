7bef833744e8c6a5cf8080e7a50cb8bb824b0457
HalyshAnton <kyhar007@gmail.com>
2025-03-13 20:13:53 +0200

    add main

--- main.py
+++ main.py
@@ -1,204 +1,143 @@
-# rate1 = Banking.exchange_rates[from_currency]
-# rate2 = Banking.exchange_rates[to_currency]
-# return amount * rate1 / rate2
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.left = None
+#         self.right = None
+#
+#
+# class BinaryTree:
+#     def __init__(self):
+#         self.root = None
+#
+#     def add(self, data):
+#         node = Node(data)
+#
+#         if self.root is None:
+#             self.root = node
+#             return
+#
+#         self._recursive_add(self.root, node)
+#
+#     def _recursive_add(self, tree_node, added_node):
+#         if added_node.data < tree_node.data:
+#             # рухаємося наліво
+#
+#             # перевіряємо чи пусто зліва
+#             if tree_node.left is None:
+#                 tree_node.left = added_node
+#                 return  # рекурсія(цикл) закінчується
+#
+#             # якщо не пусто
+#             self._recursive_add(tree_node.left, added_node)
+#         else:
+#             # рухаємося направо
+#
+#             # перевіряємо чи пусто справа
+#             if tree_node.right is None:
+#                 tree_node.right = added_node
+#                 return  # рекурсія(цикл) закінчується
+#
+#             # якщо не пусто
+#             self._recursive_add(tree_node.right, added_node)
+#
+#     def display(self):
+#         nodes_to_display = [self.root]
+#
+#         while len(nodes_to_display) != 0:
+#             # відображаємо все що є в nodes_to_display
+#             for node in nodes_to_display:
+#                 print(node.data, end=' ')
+#             print()
+#
+#             # отримуємо наступні вузли
+#             new_nodes = []
+#             for node in nodes_to_display:
+#                 if node.left is not None:
+#                     new_nodes.append(node.left)
+#
+#                 if node.right is not None:
+#                     new_nodes.append(node.right)
+#
+#             # замінюємо nodes_to_display на нові вузли
+#             nodes_to_display = new_nodes
+#
+#     def get_min(self):
+#         # отримати найменший елемент
+#
+#         node = self.root
+#
+#         while node.left is not None:
+#             node = node.left
+#
+#         return node.data
+#
+#     def find(self, num):
+#         # якщо num в корені
+#         if self.root.data == num:
+#             return True
+#
+#         return self._recursive_find(self.root, num)
+#
+#     def _recursive_find(self, tree_node, num):
+#         # числа нема
+#         if tree_node is None:
+#             return False
+#
+#         # число знайдене
+#         if tree_node.data == num:
+#             return True
+#
+#         if num < tree_node.data:
+#             return self._recursive_find(tree_node.left, num)
+#         else:
+#             return self._recursive_find(tree_node.right, num)
+#
+#
+# tree = BinaryTree()
+# tree.add(5)
+# tree.add(4)
+# tree.add(6)
+# tree.add(3)
+# tree.add(2)
+# tree.add(1)
+# tree.add(0)
+# # tree.display()
+# print(tree.get_min())
+# print(tree.find(1))
+# print(tree.find(10))
 
-import time
-import datetime
+import bintrees
 
 class Node:
     def __init__(self, data):
-        self.data = data
-        self.next = None
-        self.prev = None
-
-    def __str__(self):
-        return f"{self.data} -> {self.next}"
+        self.key = data  # вирішуємо наліво чи направо
+        self.value = None # додаткова інформація
+        self.left = None
+        self.right = None
 
 
-class DoubleLinkedList:
-    """
-    Клас двозв'язного списку.
-    """
+tree = bintrees.AVLTree()
 
-    def __init__(self):
-        """
-        Ініціалізація порожнього списку.
-        """
-        self.head = None
-        self.tail = None
-
-    def __str__(self):
-        return str(self.head)
-
-    def push_end(self, data):
-        """
-        Додає елемент у кінець списку.
-        :param data: Дані для додавання
-        """
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node
-            new_node.prev = self.tail
-            self.tail = new_node
-
-    def push_start(self, data):
-        """
-        Додає елемент на початок списку.
-        :param data: Дані для додавання
-        """
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.next = self.head
-            self.head.prev = new_node
-            self.head = new_node
-
-    def pop_end(self):
-        """
-        Видаляє останній елемент зі списку.
-        :return: Дані видаленого елемента або None, якщо список порожній
-        """
-        if not self.tail:
-            return None
-
-        data = self.tail.data
-
-        if self.head.next is None:
-            self.head = None
-            self.tail = None
-        else:
-            self.tail = self.tail.prev
-            self.tail.next = None
-
-        return data
-
-    def pop_start(self):
-        """
-        Видаляє перший елемент зі списку.
-        :return: Дані видаленого елемента або None, якщо список порожній
-        """
-
-        if not self.head:
-            return None
-
-        data = self.head.data
-
-        if self.head.next is None:
-            self.head = None
-            self.tail = None
-        else:
-            self.head = self.head.next
-            self.head.prev = None
-        return data
-
-    def is_empty(self):
-        """
-        Чи є порожній
-        :return: True якщо порожній
-        """
-        return self.head is None
-
-    def peek(self):
-        """
-        Повертає останній елемент, не видаляючи його
-        :return: останній елемент
-        """
-        return self.tail.data
+tree.insert(key=10, value='apple')
+tree.insert(key=11, value='orange')
+tree.insert(key=5, value='pear')
+tree.insert(key=9, value='melon')
+tree.insert(key=20, value='banana')
 
 
-class Message:
-    def __init__(self, text):
-        self.text = text
-        #self.time = time.time()  # кількість секунд  1970 року
-        self.time = datetime.datetime.now().time()  # нинішній чис
+# дістати елемент з id 5
+print(tree[5])
 
-    def __str__(self):
-        return f"[{self.time}] {self.text}"
+# ключі str
 
+tree = bintrees.AVLTree()
 
-# клас для обробки повідомлень
-class Messenger:
-    def __init__(self):
-        self.messages = DoubleLinkedList()
+tree.insert(key='apple', value='sweet juicy apple ')
+tree.insert(key='orange', value='orange')
+tree.insert(key='pear', value='pear')
+tree.insert(key='melon', value='melon')
+tree.insert(key='banana', value='banana')
 
-    def add_message(self, text):
-        # добавити в кінець черги
-        message = Message(text)
-
-        self.messages.push_end(message)
-
-    def read_next_message(self):
-        if self.messages.is_empty():
-            print('Повідомлень немає')
-            return
-
-        # дістає найдавніше повідемлення
-        message = self.messages.pop_start()
-
-        print(f"Читаємо повідомлення: {message}")
+print(tree['apple'])
 
 
-# m = Messenger()
-#
-# m.add_message('Hello')
-# time.sleep(1) # чекає 1 секунду
-# m.add_message("What`s up")
-# time.sleep(2) # чекає 2 секунди
-# m.add_message("I`m fine")
-#
-#
-# m.read_next_message()
-# time.sleep(1)
-# m.read_next_message()
-# time.sleep(1)
-# m.read_next_message()
-
-
-# list1 = [1, 2, 3, 4]
-# list2 = ['a', 'hello']
-# list3 = [[1, 2, 3], Node(), ]
-
-
-# пріоритетна черга
-from queue import PriorityQueue
-
-
-# створення пріоритетної черги
-queue = PriorityQueue()
-
-# добавити елемент
-symtom = 'болить голова'
-priority = 3
-
-data = (priority, symtom)
-
-queue.put(data)
-
-# одним рядком
-# queue.put((3, 'болить голова'))
-# queue.put((priority, symtom))
-
-queue.put((3, 'болить горло'))
-queue.put((1, 'струс мозку'))
-queue.put((2, 'просто спитати'))
-
-# дістати елемент
-
-data = queue.get()  # отримуєте пару пріоритет, дані
-print(data)
-
-data = queue.get()  # отримуєте пару пріоритет, дані
-print(data)
-
-data = queue.get()  # отримуєте пару пріоритет, дані
-print(data)
-
-priority, symtom = queue.get()  # отримуєте пару пріоритет, дані
-print(symtom)
-
-print(queue.empty())
